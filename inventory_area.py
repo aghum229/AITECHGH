00020b8b4d7262b674b67f0c3f8279ff32b2f1d5
aghum229 <m229@aitech-inc.co.jp>
2025-09-24 09:10:54 +0900

    Update inventory_area.py

--- inventory_area.py
+++ inventory_area.py
@@ -1525,7 +1525,6 @@ def zaiko_place():
                                     # st.write(st.session_state.df_search_result.columns)
                                     df_sorted = st.session_state.df_search_result.sort_values(by=["品番", "完了日", "移行票番号"]).reset_index(drop=True)
                                     # st.dataframe(df_sorted)
-                                    st.dataframe(df_sorted.data)
                                     st.table(df_sorted)
                                     # st.dataframe(st.session_state.df_search_result)
                                     # edited_df = st.data_editor(
