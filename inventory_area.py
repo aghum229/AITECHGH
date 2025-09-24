1b3359b3b90d5aae37fc596fc9ab64d39f65b480
aghum229 <m229@aitech-inc.co.jp>
2025-09-24 09:00:01 +0900

    Update inventory_area.py

--- inventory_area.py
+++ inventory_area.py
@@ -1525,7 +1525,7 @@ def zaiko_place():
                                     # st.write(st.session_state.df_search_result.columns)
                                     df_sorted = st.session_state.df_search_result.sort_values(by=["品番", "完了日", "移行票番号"]).reset_index(drop=True)
                                     # st.dataframe(df_sorted)
-                                    st.dataframe(df_sorted, use_container_width=True)
+                                    # st.dataframe(df_sorted, use_container_width=True)
                                     st.markdown("""
                                     <style>
                                     thead tr th {
@@ -1534,6 +1534,7 @@ def zaiko_place():
                                     }
                                     </style>
                                     """, unsafe_allow_html=True)
+                                    st.dataframe(df_sorted, use_container_width=True)
                                     # st.dataframe(st.session_state.df_search_result)
                                     # edited_df = st.data_editor(
                                     #     st.session_state.df_search_result,
