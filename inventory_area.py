43e971f5f40cf4ce9fc443bf91babdd03b0dc6da
aghum229 <m229@aitech-inc.co.jp>
2025-09-25 09:04:39 +0900

    Update inventory_area.py

--- inventory_area.py
+++ inventory_area.py
@@ -1371,7 +1371,11 @@ def zaiko_place():
                                     # st.write(st.session_state.df_search_result.columns)
                                     df_sorted = st.session_state.df_search_result.sort_values(by=["品番", "完了日", "移行票番号"]).reset_index(drop=True)
                                     # st.dataframe(df_sorted)
-                                    st.table(df_sorted)
+                                    # st.table(df_sorted)
+                                    gb = GridOptionsBuilder.from_dataframe(df_sorted)
+                                    gb.configure_grid_options(headerHeight=35)
+                                    gridOptions = gb.build()
+                                    AgGrid(df_sorted, gridOptions=gridOptions, height=200, theme="streamlit")
                                     # edited_df = st.data_editor(
                                     #     st.session_state.df_search_result,
                                     #    num_rows="dynamic",
@@ -1524,8 +1528,12 @@ def zaiko_place():
                                     # st.session_state.df_search_result.sort_values(by=["完了日", "移行票番号", "品番", "棚番"])
                                     # st.write(st.session_state.df_search_result.columns)
                                     df_sorted = st.session_state.df_search_result.sort_values(by=["品番", "完了日", "移行票番号"]).reset_index(drop=True)
-                                    st.dataframe(df_sorted)
+                                    # st.dataframe(df_sorted)
                                     # st.table(df_sorted)
+                                    gb = GridOptionsBuilder.from_dataframe(df_sorted)
+                                    gb.configure_grid_options(headerHeight=35)
+                                    gridOptions = gb.build()
+                                    AgGrid(df_sorted, gridOptions=gridOptions, height=200, theme="streamlit")
                                     # st.dataframe(st.session_state.df_search_result)
                                     # edited_df = st.data_editor(
                                     #     st.session_state.df_search_result,
