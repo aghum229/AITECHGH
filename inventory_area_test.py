db6a6450d19f54c9cf03d73f64eb0f08029fadcf
aghum229 <m229@aitech-inc.co.jp>
2025-10-01 09:03:47 +0900

    Update inventory_area_test.py

--- inventory_area_test.py
+++ inventory_area_test.py
@@ -1543,7 +1543,7 @@ def zaiko_place():
                                                         zkMo_value = "持出中"
                                                     else:
                                                         zkMo_value = ""
-                                                    st.session_state.df_search_result.loc[len(st.session_state.df_search_result)] = [item, zkMo_value, zkIko[index_2], zkHin[index_2], zkKan[index_2], round(zkSu[index_2]), zkEndDT[index_2]]
+                                                    st.session_state.df_search_result.loc[len(st.session_state.df_search_result)] = [item, zkMo_value, zkIko[index_2], zkHin[index_2], zkKan[index_2], round(int(zkSu[index_2])), zkEndDT[index_2]]
                                                     # st.write("zkHin_list:", zkHin_list)
                                                     # st.write("df_search_result:", st.session_state.df_search_result)
                                     # st.write(st.session_state.df_search_result)
@@ -1729,7 +1729,7 @@ def zaiko_place():
                                                 if zkSu[0] == "-":
                                                     zkSu_value = zkSu[0]
                                                 else:
-                                                    zkSu_value = round(zkSu[0])
+                                                    zkSu_value = round(int(zkSu[0]))
                                                 st.session_state.df_search_result.loc[len(st.session_state.df_search_result)] = [item, zkMo_value, zkIko[0], zkHin[0], zkKan[0], zkSu_value, zkEndDT[0]]
                                     # st.write(st.session_state.df_search_result)
                                     # st.session_state.df_search_result.sort_values(by=["完了日", "移行票番号", "品番", "棚番"])
