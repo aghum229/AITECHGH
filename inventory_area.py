24f14a8202e087085d12463355f2cae1f0582211
aghum229 <m229@aitech-inc.co.jp>
2026-02-06 11:24:45 +0900

    Update inventory_area.py

--- inventory_area.py
+++ inventory_area.py
@@ -2175,31 +2175,31 @@ def zaiko_place():
                     # st.write(f"#### 現在選択されている棚番 :   {st.session_state.tanaban_select_temp}")                   
                     button_key = "check_ok"
                     # st.session_state[button_key] = False
+                    @st.dialog(t["text051"])
+                    def dialog_button(button_key):
+                        global message_text
+                        # global button_key
+                        # message_text = f"""
+                        # <div style='font-size:22px; font-weight:bold;'>
+                        #     現在選択されている棚番 : <span style='font-size:30px; color:#FF0000;'>{st.session_state.tanaban_select_temp}</span><br>
+                        #     移行票番号(製造オーダー)は、<br>
+                        #     <span style='font-size:30px; color:#FF0000;'>「 {st.session_state.production_order} 」</span><br>
+                        #     でよろしいですか？
+                        # </div>
+                        # """
+                        message_text = f"""
+                        <div style='font-size:22px; font-weight:bold;'>
+                            {t["text047"]} : <span style='font-size:30px; color:#FF0000;'>{st.session_state.tanaban_select_temp}</span><br>
+                            {t["text052"]}<br>
+                            <span style='font-size:30px; color:#FF0000;'>「 {st.session_state.production_order} 」</span><br>
+                            {t["text053"]}
+                        </div>
+                        """
+                        result_flag = approve_button(message_text, button_key)
                     if st.session_state.production_order != "" and button_key not in st.session_state:
                     # if st.session_state.production_order != "" and st.session_state[button_key] == False:
                         # if st.button("棚番と移行票番号確認"):
                         # @st.dialog("棚番と移行票番号確認")
-                        @st.dialog(t["text051"])
-                        def dialog_button(button_key):
-                            global message_text
-                            # global button_key
-                            # message_text = f"""
-                            # <div style='font-size:22px; font-weight:bold;'>
-                            #     現在選択されている棚番 : <span style='font-size:30px; color:#FF0000;'>{st.session_state.tanaban_select_temp}</span><br>
-                            #     移行票番号(製造オーダー)は、<br>
-                            #     <span style='font-size:30px; color:#FF0000;'>「 {st.session_state.production_order} 」</span><br>
-                            #     でよろしいですか？
-                            # </div>
-                            # """
-                            message_text = f"""
-                            <div style='font-size:22px; font-weight:bold;'>
-                                {t["text047"]} : <span style='font-size:30px; color:#FF0000;'>{st.session_state.tanaban_select_temp}</span><br>
-                                {t["text052"]}<br>
-                                <span style='font-size:30px; color:#FF0000;'>「 {st.session_state.production_order} 」</span><br>
-                                {t["text053"]}
-                            </div>
-                            """
-                            result_flag = approve_button(message_text, button_key)
                         dialog_button(button_key)
 
                     if st.session_state.get(button_key, False):
